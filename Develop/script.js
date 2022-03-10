b1c7fcb40b09f099942dc306720982655bf891ac
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-03-09 19:54:29 -0600

    fixed issue

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,19 @@ $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 });
 
-var timesOfDay = [8, 9, 10, 11, 12, 13, 14, 15, 16];
 var currentTime = Number(moment().hours());
 console.log(currentTime);
 
 $(".time-block").each(function () {
-  var timeBlock = Number($(this).getAttribute("id"));
-  if (currentTime > timeBlock) {
-    $(this).addClass("past");
+  var timeBlock = Number($(".time-block").attr("id").replace(/time/, ""));
+  console.log(timeBlock);
+
+  if (currentTime === timeBlock) {
+    $(this).addClass("present");
+  } else if (currentTime > timeBlock) {
+    $(this).addClass("future");
+  } else if (currentTime < timeBlock) {
+    $(this).addClass("future");
   }
+  console.log(timeBlock);
 });
