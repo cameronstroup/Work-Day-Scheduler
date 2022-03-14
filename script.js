c719a9d815de3b7b6292da024873dd6d512c0bb1
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-03-13 21:16:25 -0500

    got everything working

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
   saveBtn.on("click", function (event) {
-    console.log(this.s);
-    userInput = $(this).siblings("input").val();
+    console.log(this);
+    userInput = $(this).parent().siblings(".change").children("input").val();
     console.log(userInput);
+    var times = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+    console.log(times);
+    localStorage.setItem(times, userInput);
     event.preventDefault();
   });
+  $('input[name="8-in"]').val(localStorage.getItem("8"));
+  $('input[name="9-in"]').val(localStorage.getItem("9"));
+  $('input[name="10-in"]').val(localStorage.getItem("10"));
+  $('input[name="11-in"]').val(localStorage.getItem("11"));
+  $('input[name="12-in"]').val(localStorage.getItem("12"));
+  $('input[name="1-in"]').val(localStorage.getItem("1"));
+  $('input[name="2-in"]').val(localStorage.getItem("2"));
+  $('input[name="3-in"]').val(localStorage.getItem("3"));
+  $('input[name="4-in"]').val(localStorage.getItem("4"));
+  $('input[name="5-in"]').val(localStorage.getItem("5"));
 });
 
 var currentTime = Number(moment().hours());
